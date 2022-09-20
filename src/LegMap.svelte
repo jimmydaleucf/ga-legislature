9568dc9a516cc956021ac76daa5a35fd0d924b1c
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-09-20 14:19:59 -0400

    removed api call code

--- src/LegMap.svelte
+++ src/LegMap.svelte
@@ -1,39 +1,4 @@
 <script>
-  // let arrayList = [];
-
-  // async function getData() {
-  //   const res = await fetch(
-  //     "https://v3.openstates.org/people?jurisdiction=Florida&org_classification=lower&page=1&per_page=50&apikey=f186a663-061d-462c-8364-f20e6f3594ce"
-  //   );
-  //   const results = await res.json();
-  //   const pages = results.pagination.max_page;
-  //   // console.log(pages);
-  //   let list = results.results;
-  //   console.log(list);
-  //   console.log("printing list");
-  //   arrayList.push(list);
-  //   console.log("printing arrayList");
-  //   console.log(arrayList);
-  //   if (pages > 1) {
-  //     for (let i = 2; i <= pages; i++) {
-  //       const res = await fetch(
-  //         `https://v3.openstates.org/people?jurisdiction=Florida&org_classification=lower&page=1&per_page=50&apikey=f186a663-061d-462c-8364-f20e6f3594ce&page=${i}`
-  //       );
-  //       const newBatch = await res.json();
-  //       console.log("printing newBatch");
-  //       console.log(newBatch);
-  //       const newData = newBatch.results;
-  //       console.log("printing newData");
-  //       console.log(newData);
-  //       arrayList.push(newData);
-  //       console.log("printing updated arrayList");
-  //       console.log(arrayList);
-  //     }
-  //   } else {
-  //   }
-  // }
-
-  // getData();
   import { onMount } from "svelte";
   import { query_selector_all } from "svelte/internal";
 
