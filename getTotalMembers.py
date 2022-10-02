4464ae32274646b160c4e7b79292e4e9e602e42a
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-10-02 11:41:07 -0400

    removed py files from gitignore

---
+++ getTotalMembers.py
@@ -0,0 +1,95 @@
+import requests
+import json
+
+import time
+
+path = '/Users/jdale/other-repos/ga-legislature/public/output/'
+
+# stateList = ['FL','GA']
+stateList =['Alabama',
+'Alaska',
+'Arizona',
+'Arkansas',
+'California',
+'Colorado',
+'Connecticut',
+'Delaware',
+'Florida',
+'Georgia',
+'Hawaii',
+'Idaho',
+'Illinois',
+'Indiana',
+'Iowa',
+'Kansas',
+'Kentucky',
+'Louisiana',
+'Maine',
+'Maryland',
+'Massachusetts',
+'Michigan',
+'Minnesota',
+'Mississippi',
+'Missouri',
+'Montana',
+'Nebraska',
+'Nevada',
+'New Hampshire',
+'New Jersey',
+'New Mexico',
+'New York',
+'North Carolina',
+'North Dakota',
+'Ohio',
+'Oklahoma',
+'Oregon',
+'Pennsylvania',
+'Rhode Island',
+'South Carolina',
+'South Dakota',
+'Tennessee',
+'Texas',
+'Utah',
+'Vermont',
+'Virginia',
+'Washington',
+'West Virginia',
+'Wisconsin',
+'Wyoming']
+
+output = []
+# x = input("Enter State Postal Abbreviation: ")
+# state = x
+# y = input("Which chamber? (upper or lower): ")
+# chamber = y
+
+for x in range(len(stateList)):
+    time.sleep(20)
+    state = stateList[x]
+    chamberInfo =  requests.get(f'https://v3.openstates.org/jurisdictions/{state}?include=organizations&apikey=f186a663-061d-462c-8364-f20e6f3594ce').json()
+    organizations = chamberInfo['organizations']
+    orgCount = len(organizations)
+    # print(orgCount)
+
+    chamberArray = []
+    for x in range(orgCount):
+        name =organizations[x]['name']
+        districts = organizations[x]['districts']
+        seats = len(districts)
+        whichOne = organizations[x]['classification']        
+        stateDictionary = {
+            "org": name,
+            "classification": whichOne,
+            "totalSeats": seats,
+        }
+        chamberArray.append(stateDictionary)
+
+    combined = {"state": state,
+                "organizations":chamberArray}
+    # print(combined)
+    output.append(combined)
+    print(f'********* {state} complete! ***********')
+    # print(output)
+    with open(f'{path}legislatureDataFile.json', 'w') as json_file:
+        json.dump(output, json_file)
+print('your file has been created Jimmy!')
