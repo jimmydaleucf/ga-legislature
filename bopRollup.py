2620ca27cfa2f90c0783118656569ae595cf7197
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-11-12 16:58:58 -0500

    sorting and now combined by state!

--- bopRollup.py
+++ bopRollup.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-import pprint
+import collections
 
 bopRollup = []
 directory = './public/output/incumbents/'
@@ -27,18 +27,22 @@ for file in files:
     else:
       other += 1
   incumbentTotal = gop + dem + other
+  incumbentObj = {'incumbentTotal':incumbentTotal}
   gopObj = {'gop': gop}
   demObj = {'dem':dem}
   otherObj = {'other': other}
-  partyCount.append(gopObj)
-  partyCount.append(demObj)
-  partyCount.append(otherObj)
-#   print(incumbentTotal)
-#   print(partyCount)
-  stateObj = {'state':stateName, 'chamber':chamberName, 'totalIncumbents':incumbentTotal, 'partyCount':partyCount}
-#   print(stateObj)
+#   partyCount.append(incumbentObj)
+#   partyCount.append(gopObj)
+#   partyCount.append(demObj)
+#   partyCount.append(otherObj)
+  chamberObj ={'chamber':chamberName,'incubmentTotal':incumbentTotal, 'gop':gop, 'dem':dem, 'other':other}
+  stateObj = {'state':stateName,  'chamberInfo':chamberObj}
   bopRollup.append(stateObj)
   newlist = sorted(bopRollup, key=lambda d: d['state'])
   print(newlist)
+  combinedList = collections.defaultdict(list)
+  for chamberInfo in newlist:
+    combinedList[chamberInfo['state']].append(chamberInfo['chamberInfo'])
+  newlist = [{'state': key, 'chamberInfo': value} for key, value in combinedList.items()]
 with open(f'{path}bopRollup.json', 'w') as json_file:
     json.dump(newlist, json_file)
