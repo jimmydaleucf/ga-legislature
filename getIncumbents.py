2b8ae89df8c28d62e03e2447f36d9370e5532b88
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-11-17 14:45:44 -0500

    fixed broken getIncumbents (smh)

--- getIncumbents.py
+++ getIncumbents.py
@@ -4,97 +4,56 @@
 
 
 
-# stateList = [
-# 'Alabama', 
-# 'Alaska',
-# 'Arizona',
-# 'Arkansas',
-# 'California',
-# 'Colorado',
-# 'Connecticut',
-# 'Delaware',
-# 'Florida',
-# 'Georgia',
-# 'Hawaii',
-# 'Idaho',
-# 'Illinois',
-# 'Indiana',
-# 'Iowa',
-# 'Kansas',
-# 'Kentucky',
-# 'Louisiana',
-# 'Maine',
-# 'Maryland',
-# 'Massachusetts',
-# 'Michigan',
-# 'Minnesota',
-# 'Mississippi',
-# 'Missouri',
-# 'Montana',
-# 'Nevada',
-# 'New Hampshire',
-# 'New Jersey',
-# 'New Mexico',
-# 'New York',
-# 'Florida'
-# 'North Carolina',
-# 'North Dakota',
-# 'Ohio',
-# 'Oklahoma',
-# 'Oregon',
-# 'Pennsylvania',
-# 'Rhode Island',
-# 'South Carolina',
-# 'South Dakota',
-# 'Tennessee',
-# 'Texas',
-# 'Utah',
-# 'Vermont',
-# 'Virginia',
-# 'Washington',
-# 'West Virginia',
-# 'Wisconsin',
-# 'Wyoming'
-# ]
-
-# stateList =[
-
-# ]
-
 apiKey= '&apikey=f186a663-061d-462c-8364-f20e6f3594ce'
+stateList =[
+    'Alabama', 'Wyoming', 'Florida' ]
 
-def getIncumbents(apiKey, stateList):
-    import requests
-    import json
-    import time
-    path = './public/output/incumbents/'
-    chamberList = ["lower", "upper"]
-    mainList = []
-    baseURL= 'https://v3.openstates.org/people?jurisdiction='
+# def getIncumbents(apiKey, stateList):
+import requests
+import json
+import time
+path = './public/output/incumbents/'
+chamberList = [
+    'lower',
+    'upper']
+mainList = []
+baseURL= 'https://v3.openstates.org/people?jurisdiction='
 
-    for x in range(len(stateList)):
-        state = stateList[x]
-        stateString= state.replace(" ", "_")
-        for y in range(len(chamberList)):
-            time.sleep(20)
-            chamber = chamberList[y]
-            getFirstPage = f'{baseURL}{state}&org_classification={chamber}&page=1&per_page=50{apiKey}'
-            findPageCount = requests.get(getFirstPage).json()
-            page_count = findPageCount['pagination']['max_page']
-            personList = findPageCount['results']
-            for z in range(page_count-1):
-                time.sleep(10)
+for x in range(len(stateList)):
+    state = stateList[x]
+    stateString= state.replace(" ", "_")
+    for y in range(len(chamberList)):
+        print('pausing for twenty seconds')
+        # time.sleep(20)
+        for i in range(20,0,-1):
+            print(f"{i}", end="\r", flush=True)
+            time.sleep(1)
+        chamber = chamberList[y]
+        print(chamber)
+        getFirstPage = f'{baseURL}{state}&org_classification={chamber}&page=1&per_page=50{apiKey}'
+        print(getFirstPage)
+        findPageCount = requests.get(getFirstPage).json()
+        page_count = findPageCount['pagination']['max_page']
+        print(page_count)
+        personList = findPageCount['results']
+        print(personList)
+        mainList = mainList + personList
+        print(mainList)
+        for z in range(page_count-1):
+            print('pausing for 10 seconds')
+            time.sleep(10)
+            if page_count > 1:
+                pageNum = f'page={z+2}'
+                secondaryUrl= f'{baseURL}{state}&org_classification={chamber}&page=1&per_page=50{apiKey}&{pageNum}'
+                print(secondaryUrl)
+                response = requests.get(secondaryUrl).json()
+                print(response)
+                personList = response['results']
                 mainList = mainList + personList
-                if page_count > 1:
-                    pageNum = f'page={z+2}'
-                    secondaryUrl= f'{baseURL}{state}&org_classification={chamber}&page=1&per_page=50{apiKey}&{pageNum}'
-                    response = requests.get(secondaryUrl).json()
-                    personList = response['results']
-                    mainList = mainList + personList
-                else:
-                    pass
-            with open(f'{path}{stateString}-{chamber}.json', 'w') as json_file:
-                json.dump(mainList, json_file)
-            print(f'  *** Condragulations, your file {state}-{chamber} has been updated! ***  ')
-            mainList=[]
+            else:
+                pass
+        with open(f'{path}{stateString}-{chamber}.json', 'w') as json_file:
+            json.dump(mainList, json_file)
+        print(f'  *** Condragulations, your file {state}-{chamber} has been updated! ***  ')
+        mainList=[]
 
--- rollupUpdater.py
+++ rollupUpdater.py
@@ -1,62 +1,67 @@
 import getIncumbents
 
-apiKey= '&apikey=f186a663-061d-462c-8364-f20e6f3594ce'
+apiKeys= [
+    # '&apikey=f186a663-061d-462c-8364-f20e6f3594ce', 
+    '&apikey=047dc17e-0268-4b55-9eab-3ae74457228d']
 stateList =[
-    'Alabama', 
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'Florida'
-    'North Carolina',
-    'North Dakota',
+    # 'Alabama', 
+    # 'Alaska',
+    # 'Arizona',
+    # 'Arkansas',
+    # 'California',
+    # 'Colorado',
+    # 'Connecticut',
+    # 'Delaware',
+    # 'Florida',
+    # 'Georgia',
+    'Hawaii'
+    # 'Idaho',
+    # 'Illinois',
+    # 'Indiana',
+    # 'Iowa',
+    # 'Kansas',
+    # 'Kentucky',
+    # 'Louisiana',
+    # 'Maine',
+    # 'Maryland',
+    # 'Massachusetts',
+    # 'Michigan',
+    # 'Minnesota',
+    # 'Mississippi',
+    # 'Missouri',
+    # 'Montana',
+    # 'Nevada',
+    # 'New Hampshire',
+    # 'New Jersey',
+    # 'New Mexico',
+    # 'New York',
+    # 'Florida'
+    # 'North Carolina',
+    # 'North Dakota'
     ]
 
-apiKey2 = '&apikey=047dc17e-0268-4b55-9eab-3ae74457228d'
+apiKey = '&apikey=047dc17e-0268-4b55-9eab-3ae74457228d'
 stateList2 = [
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming'
+    'Ohio'
+    # 'Oklahoma',
+    # 'Oregon',
+    # 'Pennsylvania',
+    # 'Rhode Island',
+    # 'South Carolina',
+    # 'South Dakota',
+    # 'Tennessee',
+    # 'Texas',
+    # 'Utah',
+    # 'Vermont',
+    # 'Virginia',
+    # 'Washington',
+    # 'West Virginia',
+    # 'Wisconsin',
+    # 'Wyoming'
 ]
-
+# for x in apiKeys:
+#     apiKey = x
+#     print(apiKey)
+#     print(stateList)
 getIncumbents.getIncumbents(apiKey,stateList)
-getIncumbents.getIncumbents(apiKey2,stateList2)
+   
