2c4f376b1d81b6d180bc3faca707ec7b7577800e
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-11-20 19:38:18 -0500

    set up rollupUpdater to run getIncumbents

--- rollupUpdater.py
+++ rollupUpdater.py
@@ -1,65 +1,64 @@
 import getIncumbents
 
 apiKeys= [
-    '&apikey=f186a663-061d-462c-8364-f20e6f3594ce'] 
-    # '&apikey=047dc17e-0268-4b55-9eab-3ae74457228d']
-# stateLists  =[[
-#     'Ohio',
-#     'Oklahoma',
-#     'Oregon',
-#     'Pennsylvania',
-#     'Rhode Island',
-#     'South Carolina',
-#     'South Dakota',
-#     'Tennessee',
-#     'Texas',
-#     'Utah',
-#     'Vermont',
-#     'Virginia',
-#     'Washington',
-#     'West Virginia',
-#     'Wisconsin',
-#     'Wyoming'
-#     ],[
-#     'Alabama', 
-#     'Alaska',
-#     'Arizona',
-#     'Arkansas',
-#     'California',
-#     'Colorado',
-#     'Connecticut',
-#     'Delaware',
-#     'Florida',
-#     'Georgia',
-#     'Hawaii'
-#     'Idaho',
-#     'Illinois',
-#     'Indiana',
-#     'Iowa',
-#     'Kansas',
-#     'Kentucky',
-#     'Louisiana',
-#     'Maine',
-#     'Maryland',
-#     'Massachusetts',
-#     'Michigan',
-#     'Minnesota',
-#     'Mississippi',
-#     'Missouri',
-#     'Montana',
-#     'Nevada',
-#     'New Hampshire',
-#     'New Jersey',
-#     'New Mexico',
-#     'New York',
-#     'Florida'
-#     'North Carolina',
-#     'North Dakota'
-#     ]]
+    '&apikey=f186a663-061d-462c-8364-f20e6f3594ce', 
+    '&apikey=047dc17e-0268-4b55-9eab-3ae74457228d']
 
-stateLists = [['Nebraska']]
+stateLists  =[[
+'Alabama',
+'Alaska',
+'Arizona',
+'Arkansas',
+'California',
+'Colorado',
+'Connecticut',
+'Delaware',
+'Florida',
+'Georgia',
+'Hawaii',
+'Idaho',
+'Illinois',
+'Indiana',
+'Iowa',
+'Kansas',
+'Kentucky',
+'Louisiana',
+'Maine',
+'Maryland',
+'Massachusetts',
+'Michigan',
+'Minnesota',
+'Mississippi',
+'Missouri',
+'Montana',
+'Nebraska',
+'Nevada',
+'New Hampshire'],[
+'New Jersey',
+'New Mexico',
+'New York',
+'North Carolina',
+'North Dakota',
+'Ohio',
+'Oklahoma',
+'Oregon',
+'Pennsylvania',
+'Rhode Island',
+'South Carolina',
+'South Dakota',
+'Tennessee',
+'Texas',
+'Utah',
+'Vermont',
+'Virginia',
+'Washington',
+'West Virginia',
+'Wisconsin',
+'Wyoming']]
 
-stateLists = [['Hawaii', 'Nebraska']]
+# stateLists = [['Nebraska']]
+
+# stateLists = [['Hawaii','Florida'], ['Nebraska']]
 # apiKey = '&apikey=047dc17e-0268-4b55-9eab-3ae74457228d'
 
 for x in range(len(apiKeys)):
