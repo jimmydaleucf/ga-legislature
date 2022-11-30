5b25fdf8a2dadc75976073871d1367360bcbc141
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-11-30 17:13:53 -0500

    updateChambers working for all states!

--- bopRollup.py
+++ bopRollup.py
@@ -1,79 +1,79 @@
 
-# This file takes all the individual files of incumbets saved in /public/output/incumbents/ 
-# and creates a rollup by counting all the repubs, the dems, and the others.  It creates a lightweight
-# rollup by state.  
+  # This file takes all the individual files of incumbets saved in /public/output/incumbents/ 
+  # and creates a rollup by counting all the repubs, the dems, and the others.  It creates a lightweight
+  # rollup by state.  
+def bopRollup():
+  import json
+  from pathlib import Path
+  import collections
+  import datetime
 
-import json
-from pathlib import Path
-import collections
-import datetime
+  bopRollup = []
+  directory = './public/output/incumbents/'
+  directoryTwo = 'public/output/ChambersTotal.json'
+  path = './public/output/'
 
-bopRollup = []
-directory = './public/output/incumbents/'
-directoryTwo = 'public/output/ChambersTotal.json'
-path = './public/output/'
-
-files = Path(directory).glob('*')
-for file in files:
-  f = open(file)  
-  datafile = json.load(f)
-  data = datafile['incumbents']
-  gop = 0
-  dem = 0
-  other = 0
-  stateName = data[0]['jurisdiction']['name']
-  chamberName = data[0]['current_role']['org_classification'] 
-  partyCount = []
-  for datum in data:
-    party = datum['party']
-    orgClass = datum['current_role']['org_classification']
-    if orgClass != 'executive':
-      if party == 'Republican':
-        gop += 1
-      elif party == 'Democratic' or party =='Democratic-Farmer-Labor': 
-        dem += 1
+  files = Path(directory).glob('*')
+  for file in files:
+    f = open(file)  
+    datafile = json.load(f)
+    data = datafile['incumbents']
+    gop = 0
+    dem = 0
+    other = 0
+    stateName = data[0]['jurisdiction']['name']
+    chamberName = data[0]['current_role']['org_classification'] 
+    partyCount = []
+    for datum in data:
+      party = datum['party']
+      orgClass = datum['current_role']['org_classification']
+      if orgClass != 'executive':
+        if party == 'Republican':
+          gop += 1
+        elif party == 'Democratic' or party =='Democratic-Farmer-Labor': 
+          dem += 1
+        else:
+          other += 1
       else:
-        other += 1
-    else:
-      pass
-  incumbentTotal = gop + dem + other
-  incumbentObj = {'incumbentTotal':incumbentTotal}
-  gopObj = {'gop': gop}
-  demObj = {'dem':dem}
-  otherObj = {'other': other}
-  chamberObj ={'classification':chamberName,'incubmentTotal':incumbentTotal, 'gop':gop, 'dem':dem, 'other':other}
-  stateObj = {'state':stateName,  'organizations':[chamberObj]}
-  bopRollup.append(stateObj)
-combinedTotal = open(directoryTwo)
-combinedTotalObj = json.load(combinedTotal)
-mergedList = bopRollup + combinedTotalObj
-newlist = sorted(mergedList, key=lambda d: d['state'])
-combinedList = collections.defaultdict(list)
-for organizations in newlist:
-  combinedList[organizations['state']].extend(organizations['organizations'])
-newlist = [{'state': key, 'organizations': value} for key, value in combinedList.items()]
-for state in newlist:
-    upperChamber = []
-    lowerChamber = []
-    orgs = state['organizations']
-    container = []
-    for org in orgs:
-        if org['classification'] == 'upper':
-            upperChamber.append(org)
-        elif org['classification'] == 'lower':
-            lowerChamber.append(org)
-        elif org['classification'] == 'legislature' and state['state']== 'Nebraska':
-            upperChamber.append(org)
-    if state['state'] != "Nebraska":
-        newThing = upperChamber[0] | upperChamber[1]
-        newThing2 = lowerChamber[0] | lowerChamber[1]
-        container.append(newThing2)
-    else:
-        newThing = upperChamber[0] | upperChamber[1]
-    container.append(newThing)
-    state['organizations'] = container
-now = datetime.datetime.now()
-newJson = {"timestamp":now.strftime("%m-%d-%Y %H:%M:%S"), 'states':newlist}
-with open(f'{path}bopRollup.json', 'w') as json_file:
-    json.dump(newJson, json_file)
-    print('\n✅ Your "bopRollup.json" file updated!\n')
+        pass
+    incumbentTotal = gop + dem + other
+    incumbentObj = {'incumbentTotal':incumbentTotal}
+    gopObj = {'gop': gop}
+    demObj = {'dem':dem}
+    otherObj = {'other': other}
+    chamberObj ={'classification':chamberName,'incubmentTotal':incumbentTotal, 'gop':gop, 'dem':dem, 'other':other}
+    stateObj = {'state':stateName,  'organizations':[chamberObj]}
+    bopRollup.append(stateObj)
+  combinedTotal = open(directoryTwo)
+  combinedTotalObj = json.load(combinedTotal)
+  mergedList = bopRollup + combinedTotalObj
+  newlist = sorted(mergedList, key=lambda d: d['state'])
+  combinedList = collections.defaultdict(list)
+  for organizations in newlist:
+    combinedList[organizations['state']].extend(organizations['organizations'])
+  newlist = [{'state': key, 'organizations': value} for key, value in combinedList.items()]
+  for state in newlist:
+      upperChamber = []
+      lowerChamber = []
+      orgs = state['organizations']
+      container = []
+      for org in orgs:
+          if org['classification'] == 'upper':
+              upperChamber.append(org)
+          elif org['classification'] == 'lower':
+              lowerChamber.append(org)
+          elif org['classification'] == 'legislature' and state['state']== 'Nebraska':
+              upperChamber.append(org)
+      if state['state'] != "Nebraska":
+          newThing = upperChamber[0] | upperChamber[1]
+          newThing2 = lowerChamber[0] | lowerChamber[1]
+          container.append(newThing2)
+      else:
+          newThing = upperChamber[0] | upperChamber[1]
+      container.append(newThing)
+      state['organizations'] = container
+  now = datetime.datetime.now()
+  newJson = {"timestamp":now.strftime("%m-%d-%Y %H:%M:%S"), 'states':newlist}
+  with open(f'{path}bopRollup.json', 'w') as json_file:
+      json.dump(newJson, json_file)
+      print('\n✅ Your "bopRollup.json" file updated!\n')
---
+++ updateChambers.py
@@ -0,0 +1,56 @@
+import chamberGenerator
+import bopRollup
+import json
+import os
+
+
+
+
+f = open('public/output/bopRollup.json')
+file = json.load(f)
+os.chdir('./public/diagrams/')
+
+states = file['states']
+for x in range(len(states)):
+    stateName = states[x]['state']
+    stateOrgs =states[x]['organizations']
+    for y in range(len(stateOrgs)):
+        chamberName = stateOrgs[y]['org']
+        chamberTotal =stateOrgs[y]['totalSeats']
+        filename = f'{stateName}-{chamberName}.svg'
+        dem = stateOrgs[y]['dem']
+        gop = stateOrgs[y]['gop']
+        other = stateOrgs[y]['other']
+        input_list = {
+            'parties': [
+                {
+                    'name': 'Democrat',
+                    'nb_seats':int(dem),
+                    'color': '#4165d2' ,
+                    'border_size': 0,
+                    'border_color': '#000000'
+                },
+                {
+                    'name': 'Other',
+                    'nb_seats': int(other),
+                    'color': '#e3bb1c',
+                    'border_size': 0,
+                    'border_color': '#000000'
+                },
+                {
+                    'name': 'Republican',
+                    'nb_seats': int(gop),
+                    'color': '#dc3d3d' ,
+                    'border_size': 0,
+                    'border_color': '#000000'
+                }
+            ],
+            'denser_rows': False
+        }
+        print(input_list)
+        chamberGenerator.chamberGenerator(input_list, filename)
+
+
+
+
+# chamberGenerator.chamberGenerator(input_list)
