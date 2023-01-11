4e77078fbdf9be07a6b9210aa10db934275a3f35
Jimmy Dale <jimmy.dale@warnermedia.com>
2023-01-11 17:08:18 -0500

    moved api keys to secrets file

---
+++ config.py
@@ -0,0 +1,62 @@
+import secrets
+
+apikey1 = f"{secrets.apikey1}"
+apiKey2 = f"{secrets.apikey2}"
+
+year = '2023'
+
+path = f'public/output/{year}'
+
+stateLists = [['Hawaii'], ['Nebraska']]
+
+# stateLists  =[
+# 'Alabama',
+# 'Alaska',
+# 'Arizona',
+# 'Arkansas',
+# 'California',
+# 'Colorado',
+# 'Connecticut',
+# 'Delaware',
+# 'Florida',
+# 'Georgia',
+# 'Hawaii',
+# 'Idaho',
+# 'Illinois',
+# 'Indiana',
+# 'Iowa',
+# 'Kansas',
+# 'Kentucky',
+# 'Louisiana',
+# 'Maine',
+# 'Maryland',
+# 'Massachusetts',
+# 'Michigan',
+# 'Minnesota',
+# 'Mississippi',
+# 'Missouri',
+# 'Montana',
+# 'Nebraska',
+# 'Nevada',
+# 'New Hampshire'
+# 'New Jersey',
+# 'New Mexico',
+# 'New York',
+# 'North Carolina',
+# 'North Dakota',
+# 'Ohio',
+# 'Oklahoma',
+# 'Oregon',
+# 'Pennsylvania',
+# 'Rhode Island',
+# 'South Carolina',
+# 'South Dakota',
+# 'Tennessee',
+# 'Texas',
+# 'Utah',
+# 'Vermont',
+# 'Virginia',
+# 'Washington',
+# 'West Virginia',
+# 'Wisconsin',
+# 'Wyoming']
