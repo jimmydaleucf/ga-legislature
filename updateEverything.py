ea4928cb1ba9ddc61adf807bf030ecf9915b5163
Jimmy Dale <jimmy.dale@warnermedia.com>
2023-02-09 12:27:52 -0500

    added a print 2 terminal 4 display config settings

--- updateEverything.py
+++ updateEverything.py
@@ -9,6 +9,8 @@ apiKeys = [
     f'&apikey={config.apikey1}',
     f'&apikey={config.apiKey2}']
 
+print(f'\033[96mConfig Settings:\x1B[0m\n  \033[94mYear=\x1B[0m \033[93m{config.year}\x1B[0m\n  \033[94mawsFlag set to:\x1B[0m \033[93m{config.awsFlag}\x1B[0m\n  \033[94mFiles will be saved to S3 bucket:\x1B[0m \033[93m{config.s3Bucket}\x1B[0m' )
+
 for x in range(len(apiKeys)):
     apiKey = apiKeys[x]
     stateList = config.stateLists[x]
